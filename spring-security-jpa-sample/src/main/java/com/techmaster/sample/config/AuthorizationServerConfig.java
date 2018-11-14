21cf49275cf01f9fff5df7d32a8db864b4281551
Hiep Nguyen <tahi1990@gmail.com>
2018-11-14 20:32:14 +0700

    Add token enhancer

--- spring-security-jpa-sample/src/main/java/com/techmaster/sample/config/AuthorizationServerConfig.java
+++ spring-security-jpa-sample/src/main/java/com/techmaster/sample/config/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.techmaster.sample.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +14,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -59,9 +61,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
-		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(jwtAccessTokenConverter));
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), jwtAccessTokenConverter));
 
 		endpoints.userDetailsService(userDetailsService);
 		endpoints.tokenStore(tokenStore).tokenEnhancer(tokenEnhancerChain).authenticationManager(authenticationManager);
+	}
+
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+		return new CustomerTokenEnhancer();
 	}
 }
